a85301231536d041f2226ebea5539e9cd95b9ca6
Nick Lott <nick.lott@gmail.com>
2024-05-12 09:54:50 +1200

    Prefer stdout over global variables for shell functions

--- explainshell.plugin.zsh
+++ explainshell.plugin.zsh
@@ -14,29 +14,28 @@ Plugins[explainshell_man_cmd]="man"
 
 # Convert the given command line to a URL for explainshell.com.
 #
-# Globals:
-#   URL is stored in Plugins[explainshell_url].
-#   converted query is stored Plugins[explainshell_query].
 # Arguments:
 #   $@ is Command line to convert to URL.
+# Outputs:
+#   prints URL to stdout
 cmd_to_URL(){
     # replace spaces with '+'
-    Plugins[explainshell_query]=${@:gs/ /\+}
+    local query=${@:gs/ /\+}
 
     # add base URL to query to get final URL 
-    Plugins[explainshell_url]="https://explainshell.com/explain?cmd=${Plugins[explainshell_query]}"
+    printf "https://explainshell.com/explain?cmd=${query}"
 }
 
 
 # Use lynx to get the explanation.
 #
-# Globals:
-#   URL is stored in Plugins[explainshell_url].
+# Arguments:
+#   $1 URL to fetch and format
 # Outputs:
 #   prints reformatted webpage sections to stdout
 get_explainshell_as_text(){
     # get the answer using lynx and remove the header
-    lynx $Plugins[explainshell_url] -dump -nolist -width=120 \
+    lynx "$1" -dump -nolist -width=120 \
             | sed '1,8d'
 }
 
@@ -45,14 +44,13 @@ get_explainshell_as_text(){
 #
 # Globals:
 #   $BUFFER contains the current command line.
-#   URL is stored in Plugins[explainshell_url].
 # Outputs:
 #   prints to stdout
 explainshell_cmd() {
-    cmd_to_URL $BUFFER
-    printf "\nGetting '${Plugins[explainshell_url]}'...\n" 
-    get_explainshell_as_text 
-    printf  "\n>> $BUFFER <<\n"
+    local url=$(cmd_to_URL ${BUFFER})
+    printf "\nGetting '${url}'...\n" 
+    get_explainshell_as_text "${url}"
+    printf  "\n>> ${BUFFER} <<\n"
     printf "\nPress Enter to execute or <CTRL-C> to abort"
 }
 
@@ -66,20 +64,20 @@ explainshell_cmd() {
 explainshell_cmd_window() {
     temp_file=$(mktemp)
 
-    cmd_to_URL $BUFFER
-    get_explainshell_as_text > $temp_file
-    tmux splitw "cat $temp_file | less"
+    local url=$(cmd_to_URL $BUFFER)
+    get_explainshell_as_text "${url}" > ${temp_file}
+    tmux splitw "cat ${temp_file} | less"
     #dialog --textbox $temp_file 0 0 
     
-    [ -f $temp_file ] && rm $temp_file
+    [ -f ${temp_file} ] && rm ${temp_file}
 }
 
 # Open the explanation in system web browser.
 # Globals:
 #   $BUFFER contains the current command line.
 explainshell_cmd_browser(){
-    cmd_to_URL $BUFFER
-    $Plugins[explainshell_browser_cmd] "${Plugins[explainshell_url]}"
+    local url=$(cmd_to_URL $BUFFER)
+    $Plugins[explainshell_browser_cmd] "${url}"
 }
 
 # Get the man page for the first item on the command line 
